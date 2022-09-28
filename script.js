33a7113ef4ec3b28515d3f3cce64915d4b38d720
Dempoleon <dempoleon@gmail.com>
2022-09-27 21:58:17 -0700

    created generatePassword function, prompts user for length and confirms desired password criteria

--- script.js
+++ script.js
@@ -3,12 +3,36 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = generatePassword(); //create the generatePassword Function
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function generatePassword() {
+  // prompt the user for the length of the desired password
+  // password must be over 8 characters long
+  passLength = prompt("How many characters would you like your password to contain?");
+  // check to see if the user input is a number
+  if (isNaN(passLength)) {
+    alert('Not a number');
+    return;
+  } else if (passLength < 8) {
+    alert('password less than 8');
+    return;
+  } 
+  
+  // confirm which password criteria the user wants
+  var specialChar = confirm('Do you want special characters?');
+  console.log(specialChar);
+  var numericChar = confirm('Do you want numeric characers?');
+  console.log(numericChar);
+  var lowerCase = confirm('Do you want lower case characters?');
+  console.log(lowerCase);
+  var upperCase = confirm('Do you want upper case characters?');
+  console.log(upperCase);
+
+}
